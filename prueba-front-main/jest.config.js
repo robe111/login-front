0384d319329d85e2dbb98e2e1a6fb4340f6160ff
robe111 <rober_decastro@hotmail.com>
2023-11-13 17:39:09 +0100

    Delete prueba-front-main directory

--- prueba-front-main/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/setupJest.ts"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
-};
